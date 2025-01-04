88be313a8d08e362ce60970e16a24cf93e63a888
Ufuk Paşaalioğlu <Ufukpasaalioglu@gmail.com>
2025-01-05 00:54:50 +0300

    Level files added
    Files related to the level structure were created

---
+++ Assets/Scripts/Game Logic/LevelData.cs
@@ -0,0 +1,11 @@
+[System.Serializable]
+public class LevelData
+{
+    public int level_number;
+    public int grid_rows;
+    public int grid_columns;
+    public int colors_count;
+    public int last_default_icon_index;
+    public int last_first_icon_index;
+    public int last_second_icon_index;
+}
